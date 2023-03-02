8f60acefa74204476a389857825059b62b9d2bc9
SpeedWaWhale <102420183+SpeedWaWhale@users.noreply.github.com>
2023-03-02 15:35:45 +0100

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import 'zone.js/dist/zone';
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+const ffmpeg = createFFmpeg({ log: true });
 
 @Component({
   selector: 'my-app',
@@ -11,6 +13,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
   <video #preview width="1080" height="720" autoplay muted></video>
     <button (click)="start()">TEST</button>
     <a #download href="#">Download</a>
+    <h1>RecordedMP4</h1>
+    <video #mp4recorded width="1080" height="720" controls></video>
     <video #recorded width="1080" height="720" controls></video>
     <canvas #output></canvas>
   `,
@@ -18,6 +22,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 export class App {
   @ViewChild('preview', { static: false }) previewElem: ElementRef;
   @ViewChild('recorded', { static: false }) recordedElem: ElementRef;
+  @ViewChild('mp4recorded', { static: false }) recordedMP4Elem: ElementRef;
   @ViewChild('download', { static: false }) downloadElem: ElementRef;
   @ViewChild('output', { static: false }) canvasElem: ElementRef;
 
@@ -59,6 +64,11 @@ export class App {
     return stop;
   }
 
+  getVideoTrack(video) {
+    const [track] = video.captureStream().getVideoTracks();
+    return track;
+  }
+
   start() {
     this.streamScreen()
       .then((stream) => {
@@ -81,6 +91,32 @@ export class App {
         this.recordedElem.nativeElement.src = URL.createObjectURL(videoBlock);
         this.downloadElem.nativeElement.href =
           this.recordedElem.nativeElement.src;
+        return videoChunks;
+      })
+      .then(async (videoChunks) => {
+        //MP4 converter
+        await ffmpeg.load();
+        ffmpeg.FS(
+          'writeFile',
+          'testMp4',
+          await fetchFile(
+            new Uint8Array(
+              await new Blob(videoChunks as BlobPart[]).arrayBuffer()
+            )
+          )
+        );
+        await ffmpeg.run('-i', 'testMp4', 'output.mp4');
+        const data = ffmpeg.FS('readFile', 'output.mp4');
+        this.recordedMP4Elem.nativeElement.src = URL.createObjectURL(
+          new Blob([data.buffer], { type: 'video/mp4' })
+        );
+      })
+      .then(async () => {
+        await this.recordedElem.nativeElement.play();
+        console.log(
+          'getTracks',
+          this.getVideoTrack(this.recordedElem.nativeElement)
+        );
       })
       .then(() => {
         // Draw into canvas
